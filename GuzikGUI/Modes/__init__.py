a0ddf6d8eb91b82674b007d57d1302d574b7265d
Alexandre Dumont <alexandre.dumont3@usherbrooke.ca>
2023-06-26 10:52:10 -0400

    Initial release.

--- GuzikGUI/Modes/__init__.py
+++ GuzikGUI/Modes/__init__.py
@@ -1,16 +1,16 @@
-from Modes.Mode import BlankMode
-from Modes.Mode import dummy_guzik
+from GuzikGUI.Modes.Mode import BlankMode
+from GuzikGUI.Modes.Mode import dummy_guzik
 
-from Modes.TimeDomain import TimeSeries
-from Modes.TimeDomain import OneDimHistogram
-from Modes.TimeDomain import TwoDimHistogram
+from GuzikGUI.Modes.TimeDomain import TimeSeries
+from GuzikGUI.Modes.TimeDomain import OneDimHistogram
+from GuzikGUI.Modes.TimeDomain import TwoDimHistogram
 
-from Modes.FrequencyDomain import VoltageSpectrum
-from Modes.FrequencyDomain import PowerSpectrum
+from GuzikGUI.Modes.FrequencyDomain import VoltageSpectrum
+from GuzikGUI.Modes.FrequencyDomain import PowerSpectrum
 
 try:
-    from Modes.FrequencyDomain import VoltageSpectrumCUDA
-    from Modes.FrequencyDomain import PowerSpectrumCUDA
-    from Modes.FrequencyDomain import CrossPowerSpectrumCUDA
+    from GuzikGUI.Modes.FrequencyDomain import VoltageSpectrumCUDA
+    from GuzikGUI.Modes.FrequencyDomain import PowerSpectrumCUDA
+    from GuzikGUI.Modes.FrequencyDomain import CrossPowerSpectrumCUDA
 except:
     pass
--- GuzikGUI/__init__.py
+++ GuzikGUI/__init__.py
@@ -1,4 +1,5 @@
-from .GuzikGUI import *
+from .GuzikGUI import launch
+del GuzikGUI
 
 if __name__ == '__main__':
     app, win = launch()

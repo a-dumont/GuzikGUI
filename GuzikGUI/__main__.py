62ccdb836f3d97cbb55a31e742bcddc676603179
Alexandre Dumont <56271618+a-dumont@users.noreply.github.com>
2023-06-26 11:09:28 -0400

    Working (#5)

---
+++ GuzikGUI/__main__.py
@@ -0,0 +1,8 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from GuzikGUI.GuzikGUI import GuzikOScopeWindow
+
+app = QApplication(sys.argv)
+win = GuzikOScopeWindow()
+win.show()
+sys.exit(app.exec())
